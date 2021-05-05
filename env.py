92ae37877279251f591dc296dc50e45be198c943
Liam Jackson <Ljackson3613@yahoo.com>
2021-05-05 12:00:12 -0500

    _

---
+++ env.py
@@ -0,0 +1,24 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Private info
+
+# IP adress for the server
+host = "157.230.209.171"
+# Username
+user = "easley_1272"
+# Password
+password = "myyniY3EoU68aV3bufayOOI9k0H39Oic"
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Use this to get into SQL db
+
+# import pandas as pd
+# import env
+
+# def get_connection(db, user=env.user, host=env.host, password=env.password):
+    # return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+# df = pd.read_sql('SELECT * FROM properties_2017', get_connection('zillow'))
+
+# df.head(5)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- prepare.py
+++ prepare.py
@@ -39,3 +39,119 @@ def basic_clean(article):
 
 #-------------------------------------------------------------------------------------------------------------------------------
 
+def tokenize(article):
+    '''
+    This function takes in an article as a string.
+    Creates a tokenizer using nltk.
+    Uses the tokenize on the artical and returns the article in string fromat.
+    '''
+    # Create the tokenizer
+    tokenizer = nltk.tokenize.ToktokTokenizer()
+    
+    tokenizer.tokenize(article, return_str = True)
+    
+    return article
+
+#-------------------------------------------------------------------------------------------------------------------------------
+
+def stem(article):
+    '''
+    This function takes in an article as a string.
+    Creates a porter stemmer.
+    Applies the stemmer to each word in the article/string.
+    Joins the stems into a single string called article_stemmed.
+    Returns article_stemmed with all stemed characters. 
+    '''
+    # Create porter stemmer.
+    ps = nltk.porter.PorterStemmer()
+    
+    # Apply the stemmer to each word in our string.
+    stems = [ps.stem(word) for word in article.split()]
+    
+    # Join stems
+    article_stemmed = ' '.join(stems)
+    
+    return article_stemmed
+
+#-------------------------------------------------------------------------------------------------------------------------------
+
+def lemmatize(article):
+    '''
+    This function takes in an article as a string.
+    Downloads 'wordnet' from nltk.
+    Creates a lemmatizer.
+    Applies the lemmatizer to each word in the article/string.
+    Joins the lemmmas into a single string called article_lemmatized.
+    Returns artical_lemmatized with all lemmatized characters. 
+    '''
+    # Download the first time.
+    nltk.download('wordnet')
+    
+    # Create the Lemmatizer.
+    wnl = nltk.stem.WordNetLemmatizer()
+    
+    # Apply the lemmatize to each word in our string.
+    lemmas = [wnl.lemmatize(word) for word in article.split()]
+    
+    # Join lemmas
+    article_lemmatized = ' '.join(lemmas)
+    
+    return article_lemmatized
+
+#-------------------------------------------------------------------------------------------------------------------------------
+
+def remove_stopwords(article, extra_words = [], exclude_words = []):
+    '''
+    This function takes in a string, optional extra_words and exclude_words parameters
+    with default empty lists and returns a string.
+    '''
+    # Create stopword_list.
+    stopword_list = stopwords.words('english')
+    
+    # Remove 'exclude_words' from stopword_list to keep these in my text.
+    stopword_list = set(stopword_list) - set(exclude_words)
+    
+    # Add in 'extra_words' to stopword_list.
+    stopword_list = stopword_list.union(set(extra_words))
+    
+    # Split words in string.
+    words = article.split()
+    
+    # Create a list of words from my string with stopwords removed and assign to variable.
+    filtered_words = [word for word in words if word not in stopword_list]
+    
+    # Join words in the list back into strings and assign to a variable.
+    string_without_stopwords = ' '.join(filtered_words)
+    
+    return string_without_stopwords
+
+#-------------------------------------------------------------------------------------------------------------------------------
+
+def prep_article_data(df, column, extra_words=[], exclude_words=[]):
+    '''
+    This function take in a df and the string name for a text column with 
+    option to pass lists for extra_words and exclude_words and
+    returns a df with the text article title, original text, stemmed text,
+    lemmatized text, cleaned, tokenized, & lemmatized text with stopwords removed.
+    '''
+    df['clean'] = df[column].apply(basic_clean)\
+                            .apply(tokenize)\
+                            .apply(remove_stopwords, 
+                                   extra_words=extra_words, 
+                                   exclude_words=exclude_words)
+    
+    df['stemmed'] = df[column].apply(basic_clean)\
+                            .apply(tokenize)\
+                            .apply(stem)\
+                            .apply(remove_stopwords, 
+                                   extra_words=extra_words, 
+                                   exclude_words=exclude_words)
+    
+    df['lemmatized'] = df[column].apply(basic_clean)\
+                            .apply(tokenize)\
+                            .apply(lemmatize)\
+                            .apply(remove_stopwords, 
+                                   extra_words=extra_words, 
+                                   exclude_words=exclude_words)
+    
+    return df[['title', column,'clean', 'stemmed', 'lemmatized']]
